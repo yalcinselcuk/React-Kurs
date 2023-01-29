d23b68bfbc38d1dc8d2da20f625a47a8706e445f
yalcinselcuk <yalcinselcukkk@outlook.com>
2023-01-29 20:31:11 +0300

    navbar add ve sepete ürün ekleyince sayi artiyor

--- reactProject/intro/src/Navi.js
+++ reactProject/intro/src/Navi.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
 export default class Navi extends Component {
+
+  constructor(props){
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen : false
+    };
+  }
+
+  toggle(){
+    this.setState({
+      isOpen:!this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Navi</h1>
-      </div>
+      <Navbar>
+        <NavbarBrand href="/">Online Alışveriş Sitesi</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options - {this.props.sepet.length}
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
     )
   }
 }
