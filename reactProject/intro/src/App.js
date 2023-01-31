66f3880fa79f5c11c9aad0f67b27ceaadafb245e
yalcinselcuk <yalcinselcukkk@outlook.com>
2023-01-31 09:30:02 +0300

    Routing İmplementasyonu

--- reactProject/intro/src/App.js
+++ reactProject/intro/src/App.js
@@ -4,50 +4,53 @@ import ProductList from "./ProductList";
 import { Container, Row, Col } from 'reactstrap';
 import React, { Component } from 'react'
 import alertify from "alertifyjs";
+import { Route, Routes } from "react-router-dom";
+import NotFound from './NotFound';
+import SepetDetayı from "./SepetDetayı";
 
 
 
 export default class App extends Component {
-  
-  state = {currentCategory : "", products : [], sepet : []};
+
+  state = { currentCategory: "", products: [], sepet: [] };
 
   changeCategory = (category) => {
     this.setState({ currentCategory: category.categoryName })
     this.getProducts(category.id);
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getProducts();
   }
-  getProducts = (categoryId) =>{
+  getProducts = (categoryId) => {
 
     let url = "http://localhost:3000/products";
-    if(categoryId){//eger bu seoUrl tanimlanmissa
-      url+="?categoryId="+categoryId;
+    if (categoryId) {//eger bu seoUrl tanimlanmissa
+      url += "?categoryId=" + categoryId;
     }
 
     //api'ye fetch uzerinden erisiyoruz
     fetch(url)
-    .then(response => response.json())//gelen istegi json 'a donustur
-    .then(data => this.setState({products : data}));;//o istek sonucunda gelen dataları attik
+      .then(response => response.json())//gelen istegi json 'a donustur
+      .then(data => this.setState({ products: data }));;//o istek sonucunda gelen dataları attik
     //yukaridan asagiya katmanli calisti
 
   }
-  sepeteEkle=(product)=>{
+  sepeteEkle = (product) => {
     let yeniSepet = this.state.sepet;
     //tekrar edip etmediğini hesapladık
     var itemEkle = yeniSepet.find(c => c.product.id === product.id);
-    if(itemEkle){
-      itemEkle.quantity+=1;
-    }else{
-      yeniSepet.push({product : product, quantity : 1});
+    if (itemEkle) {
+      itemEkle.quantity += 1;
+    } else {
+      yeniSepet.push({ product: product, quantity: 1 });
     }
-    this.setState({sepet : yeniSepet});
-    alertify.success(product.productName + " Sepete Eklendi");
+    this.setState({ sepet: yeniSepet });
+    alertify.success(product.productName + " Sepete Eklendi", 2);//2 olarak belirttiğimiz ekranda kalma süresi. sn cinsinden
   }
-  sepettenSil=(product)=>{
+  sepettenSil = (product) => {
     let yeniSepet = this.state.sepet.filter(c => c.product.id !== product.id)
-    this.setState({sepet : yeniSepet})
+    this.setState({ sepet: yeniSepet })
   }
   render() {
 
@@ -58,17 +61,34 @@ export default class App extends Component {
       <div>
         <Container>
           <Row>
-
-            <Navi sepettenSil={this.sepettenSil} sepet = {this.state.sepet}/>
+            <Navi sepettenSil={this.sepettenSil} sepet={this.state.sepet} />
           </Row>
-          
           <Row>
             {/* xs dediğimiz = sayfamız 12 alana ayrıldığından hangi alanda olduğunu belirttik */}
             <Col xs="3">
-              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo}/>
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
-              <ProductList sepeteEkle = {this.sepeteEkle} products = {this.state.products} currentCategory={this.state.currentCategory} info={productInfo}/>
+              <Routes>
+                <Route
+                  exact
+                  path="/"
+                  element={
+                    <ProductList
+                      products={this.state.products}
+                      sepeteEkle={this.sepeteEkle}
+                      currentCategory={this.state.currentCategory}
+                      info={productInfo}
+                    />
+                  } /> {/*localhost direk çalışınca ProductList'in default olarak çalışmasını istiyoruz ve exact'da direk gelmesini sağlıyor */}
+                <Route exact path="/sepet" element={<SepetDetayı />} />
+                <Route element={<NotFound />} />{/* Routes bütün Route'ları tek tek gezdi ve hiçbiri uymazsa en sondakini çalıştırdı*/}
+              </Routes>
+
             </Col>
           </Row>
         </Container>
---
+++ reactProject/intro/src/SepetDetayı.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SepetDetayı extends Component {
+  render() {
+    return (
+      <div>SepetDetayı</div>
+    )
+  }
+}
