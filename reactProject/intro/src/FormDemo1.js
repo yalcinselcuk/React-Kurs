33f6026b515819f52cb794ae513f314763aec47a
yalcinselcuk <yalcinselcukkk@outlook.com>
2023-01-31 11:33:09 +0300

    Çoklu Form Elemanlarıyla Çalışmak

--- reactProject/intro/src/FormDemo1.js
+++ reactProject/intro/src/FormDemo1.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { Form } from 'reactstrap'
 
 export default class FormDemo1 extends Component {
-    state = { username: "" }
+    state = { username: "", city:"" }
 
     onChangeHandler = (event) =>{
-        this.setState({username : event.target.value})
+        //this.setState({username : event.target.value})
         /*event.target : event'e sebep olan nesne.Burada da input tetikliyor */
+        //üstteki yazılan kod tek bir input için doğru.Eger birden fazla input varsa aşağıdaki kod kullanılır
+
+        let name = event.target.name; //event i tetikleyen nesnenin adını aldık
+        let value = event.target.value;
+        this.setState({[name]:value})
     }
 
     onSubmitHandler = (event) =>{
@@ -18,9 +23,16 @@ export default class FormDemo1 extends Component {
         return (
             <div>
                 <Form onSubmit={this.onSubmitHandler}>
+
                     <h3>Username</h3>
-                    <input onChange={this.onChangeHandler} type="text"></input>
-                    <h4>Username : {this.state.username}</h4>
+                    <input name = "username" onChange={this.onChangeHandler} type="text"></input>
+                    <h3>Username : {this.state.username}</h3>
+
+                    <h3>City</h3>
+                    <input name = "city" onChange={this.onChangeHandler} type="text"></input>
+                    <h3>City : {this.state.city}</h3>
+                    {/*input'larda name'lere verilenler ile this.state'te kullanılanlar aynı olmalı */}
+
                     <input type="submit" value="Kaydet"></input>
                 </Form>
             </div>
