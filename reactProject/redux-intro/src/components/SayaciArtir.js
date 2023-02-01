bcfb01d6f204fb3a13b5906ad73785ef2e7c414c
yalcinselcuk <yalcinselcukkk@outlook.com>
2023-02-01 12:05:53 +0300

    dispatch ile çalışmak

--- reactProject/redux-intro/src/components/SayaciArtir.js
+++ reactProject/redux-intro/src/components/SayaciArtir.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { sayaciArtir } from '../redux/actions/counterActions'
 
-export default class SayaciArtir extends Component {
+class SayaciArtir extends Component {
   render() {
     return (
       <div>
-        
+        <button onClick={e => {
+          this.props.dispatch(sayaciArtir());
+        }}>
+          1 Artır
+        </button>
       </div>
     )
   }
 }
+
+//dispatch : bizim buradaki çağırmak istediğimiz aksiyonumuz 
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(sayaciArtir, dispatch) }
+  //bindActionCreators : aksiyonu bağlamak için kullanılan bir araç
+  //içinde çağırdığımız aksiyon metodumuz counterAction içindeki
+}
+
+export default connect(mapDispatchToProps)(SayaciArtir)
--- reactProject/redux-intro/src/components/SayaciAzalt.js
+++ reactProject/redux-intro/src/components/SayaciAzalt.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { sayaciAzalt } from '../redux/actions/counterActions';
 
-export default class SayaciAzalt extends Component {
+ class SayaciAzalt extends Component {
   render() {
     return (
       <div>
-
+        <button onClick={e => {
+          this.props.dispatch(sayaciAzalt());
+        }}>
+          1 Azalt
+        </button>
       </div>
     )
   }
 }
+
+//dispatch : bizim buradaki çağırmak istediğimiz aksiyonumuz 
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(sayaciAzalt, dispatch) }
+  //bindActionCreators : aksiyonu bağlamak için kullanılan bir araç
+  //içinde çağırdığımız aksiyon metodumuz counterAction içindeki
+}
+
+export default connect(mapDispatchToProps)(SayaciAzalt)
