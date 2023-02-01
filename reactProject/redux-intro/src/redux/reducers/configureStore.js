38eab4d23c8a7c07e4729742d499add13c54acde
yalcinselcuk <yalcinselcukkk@outlook.com>
2023-02-01 10:54:57 +0300

    configureStore'da mağaza oluşturuldu

--- reactProject/redux-intro/src/redux/reducers/configureStore.js
+++ reactProject/redux-intro/src/redux/reducers/configureStore.js
@@ -3,3 +3,7 @@ import reducers from "./index";
 //bu yapının tamamını içeren yapı : store
 //bu store'ı react'e bağlamak kaldı
 //onu da normal projedeki index.js'nin içine gidip hallediyoruz
+
+export default function configureStore(){
+    return createStore(reducers);
+}
